24711cc31f8ff70f8ab4836090ce71bbea678469
Omar Brikaa <brikaaomar@gmail.com>
2023-05-06 23:05:51 +0300

    Program

---
+++ program.js
@@ -0,0 +1,71 @@
+class Model {
+  #entities;
+  constructor(...initial_entities) {
+    // Variable parameters using rest syntax
+    this.#entities = initial_entities;
+  }
+
+  select(selection_fn) {
+    // Subprogram as parameter
+    return this.#entities.filter(selection_fn);
+  }
+}
+
+class DeletedEntity {
+  constructor(date) {
+    this.deletion_date = date;
+  }
+}
+
+class Entity {
+  toString() {
+    console.log('Entity');
+  }
+
+  delete() {
+    // Dynamic inheritance. When used in the child, the child's parent will be DeletedEntity instead of Entity
+    Object.setPrototypeOf(this, new DeletedEntity(new Date()));
+  }
+}
+
+class User extends Entity {
+  constructor(name) {
+    super();
+    this.name = name;
+  }
+
+  toString() {
+    return `User called ${this.name}`;
+  }
+}
+
+class Room extends Entity {
+  constructor(number) {
+    super();
+    this.number = number;
+  }
+
+  toString() {
+    return `Room ${this.number}`;
+  }
+}
+
+function logEntities(logging_fn, ...entities) {
+  // Subprogram as parameter
+  // Variable parameters
+  // Also entities could contain Users and/or Rooms (polymorphism)
+  entities.forEach((e) => logging_fn(e.toString()));
+}
+
+(() => {
+  userModel = new Model(new User('Omar'), new User('Adel'), new User('Abdel Hamid'));
+  roomModel = new Model(new Room(32), new Room(35), new Room(23));
+  logEntities(
+    console.log,
+    ...userModel.select((u) => u.name.startsWith('A')),
+    ...roomModel.select((r) => r.number >= 30)
+  );
+  const omar = userModel.select((u) => u.name === 'Omar')[0];
+  omar.delete();
+  console.log(omar.deletion_date);
+})();
